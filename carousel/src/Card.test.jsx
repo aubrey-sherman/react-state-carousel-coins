dc1064c75247791502738cd0e854111e8176eea1
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-05-28 19:35:15 -0400

    Wrote a smoke test for Card

---
+++ carousel/src/Card.test.jsx
@@ -0,0 +1,16 @@
+import { it, expect } from "vitest";
+
+import { render, fireEvent } from "@testing-library/react";
+import Card from "./Card.jsx";
+import TEST_IMAGES from "./_testCommon.js";
+
+const { caption, src } = TEST_IMAGES[0];
+
+it("renders without crashing", function () {
+  render(<Card
+    caption={caption}
+    src={src}
+    currNum={1}
+    totalNum={1}
+  />);
+});
